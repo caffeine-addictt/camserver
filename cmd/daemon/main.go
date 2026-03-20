4b221180065fe36d4c923cbae5b82190f188218d
AlexNg <contact@ngjx.org>
2026-03-21 01:27:54 +0800

    feat: Add verbosity flags
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -7,14 +7,24 @@ import (
 )
 
 var rootCmd = &cobra.Command{}
+// Higher verbosity = more log output
+var (
+	verbosity int
+	quiet     bool
+)
 
 func init() {
 	cmd.AddManPagesCmd(rootCmd)
 	cmd.AddVersionCmd(rootCmd)
+
+	rootCmd.PersistentFlags().CountVarP(&verbosity, "verbosity", "v", "verbosity level (-v|-vv)")
+	rootCmd.PersistentFlags().BoolVarP(&quiet, "quiet", "q", false, "quiet mode")
+	rootCmd.MarkFlagsMutuallyExclusive("verbosity", "quiet")
 }
 
 func main() {
 	defer log.Sync()
+
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal().Msg(err.Error()).Send()
 	}
