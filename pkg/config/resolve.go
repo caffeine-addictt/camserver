56f35b366984eb92526117fc9e8057456ef84b4b
AlexNg <contact@ngjx.org>
2026-03-21 04:29:31 +0800

    feat: Return a path
    Signed-off-by: AlexNg <contact@ngjx.org>

--- pkg/config/resolve.go
+++ pkg/config/resolve.go
@@ -33,12 +33,21 @@ func loadConfigFile(path string) (*Config, error) {
 	return &cfg, nil
 }
 
-func LoadConfig(customPath *string) (*Config, error) {
+// LoadConfig looks at the following paths and attempts
+// to locate the first usable config file
+//
+//   - `-C` flag
+//   - $pwd
+//   - $HOME/.camserver.yaml
+//   - $XDG_CONFIG_HOME/camserver/camserver.yaml
+//
+// Returns cfg, path used and any errors
+func LoadConfig(customPath *string) (*Config, string, error) {
 	if customPath != nil {
 		cfg, err := loadConfigFile(*customPath)
 		if err == nil {
 			log.Info().WithMeta("scope", "cfg").Msgf("loaded '%s'", *customPath).Send()
-			return cfg, nil
+			return cfg, *customPath, nil
 		}
 		log.Error().WithMeta("scope", "cfg").Msgf("failed to load '%s': %v", *customPath, err).Send()
 	}
@@ -63,7 +72,7 @@ func LoadConfig(customPath *string) (*Config, error) {
 		cfg, err := loadConfigFile(location)
 		if err == nil {
 			log.Info().WithMeta("scope", "cfg").Msgf("loaded '%s'", location).Send()
-			return cfg, nil
+			return cfg, location, nil
 		}
 		log.Debug().WithMeta("scope", "cfg").Msgf("failed to load '%s': %v", location, err).Send()
 	}
