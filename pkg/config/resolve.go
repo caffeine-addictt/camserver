5a04d97c7494483830679926553919c88d0b77ca
AlexNg <contact@ngjx.org>
2026-03-21 04:15:11 +0800

    feat: resolving cfg file
    Signed-off-by: AlexNg <contact@ngjx.org>

---
+++ pkg/config/resolve.go
@@ -0,0 +1,72 @@
+package config
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/goccy/go-yaml"
+	"github.com/lattesec/log"
+)
+
+const (
+	DefaultFileName      = "camserver.yaml"
+	DefaultConfigDirName = "camserver"
+)
+
+func loadConfigFile(path string) (*Config, error) {
+	f, err := os.OpenFile(filepath.Clean(path), os.O_RDONLY, 0o600)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = f.Close() }()
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
+
+func LoadConfig(customPath *string) (*Config, error) {
+	if customPath != nil {
+		cfg, err := loadConfigFile(*customPath)
+		if err == nil {
+			log.Info().WithMeta("scope", "cfg").Msgf("loaded '%s'", *customPath).Send()
+			return cfg, nil
+		}
+		log.Error().WithMeta("scope", "cfg").Msgf("failed to load '%s': %v", *customPath, err).Send()
+	}
+
+	searchLocations := []string{"."}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Error().WithMeta("scope", "cfg").Msgf("failed to get user home dir: %v", err).Send()
+	} else {
+		searchLocations = append(searchLocations, filepath.Join(homeDir, DefaultFileName))
+	}
+
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		log.Error().WithMeta("scope", "cfg").Msgf("failed to get user config dir: %v", err).Send()
+	} else {
+		searchLocations = append(searchLocations, filepath.Join(configDir, DefaultConfigDirName, DefaultFileName))
+	}
+
+	for _, location := range searchLocations {
+		cfg, err := loadConfigFile(location)
+		if err == nil {
+			log.Info().WithMeta("scope", "cfg").Msgf("loaded '%s'", location).Send()
+			return cfg, nil
+		}
+		log.Debug().WithMeta("scope", "cfg").Msgf("failed to load '%s': %v", location, err).Send()
+	}
+
+	return &Config{}, nil
+}
