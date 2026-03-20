89a34c0c52f11a410dc5655ca19dae0e891aa5b6
AlexNg <contact@ngjx.org>
2026-03-21 02:31:29 +0800

    feat: Add web main
    Signed-off-by: AlexNg <contact@ngjx.org>

---
+++ cmd/web/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/caffeine-addictt/camserver/cmd"
+	"github.com/caffeine-addictt/camserver/internal/util"
+	"github.com/lattesec/log"
+)
+
+func main() {
+	defer log.Sync()
+
+	rootCmd, err := cmd.GetRootCmd()
+	if err != nil {
+		log.Fatal().Msg(err.Error()).Send()
+	}
+
+	rootCmd.Use = "camserver"
+	rootCmd.Short = "camserver web"
+	rootCmd.Long = util.MultilineString(
+		"Camera Server Web Interface",
+		"",
+		"Access bridge to camserver-daemon",
+	)
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal().Msg(err.Error()).Send()
+	}
+}
