6495910932ad3bd237567400aa7219e30814c721
AlexNg <contact@ngjx.org>
2026-03-21 02:29:19 +0800

    feat: Move rootCmd gen to standard
    Signed-off-by: AlexNg <contact@ngjx.org>

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -4,54 +4,23 @@ import (
 	"github.com/caffeine-addictt/camserver/cmd"
 	"github.com/caffeine-addictt/camserver/internal/util"
 	"github.com/lattesec/log"
-	"github.com/spf13/cobra"
 )
-
-// Higher verbosity = more log output
-var (
-	verbosity int
-	quiet     bool
-)
-
-var rootCmd = &cobra.Command{
-	Use:           "camserver-daemon",
-	Short:         "camserver daemon",
-	SilenceErrors: true,
-	SilenceUsage:  true,
-	Long: util.MultilineString(
-		"Camera Server daemon",
-		"",
-		"Handles everything in the backend.",
-	),
-	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		var verbosityToSet log.Level
-		switch verbosity {
-		case 0:
-			verbosityToSet = log.WARN
-		case 1:
-			verbosityToSet = log.INFO
-		default:
-			verbosityToSet = log.DEBUG
-		}
-		if quiet {
-			verbosityToSet = log.QUIET
-		}
-
-		return log.DefaultLogger().SetLevel(verbosityToSet)
-	},
-}
-
-func init() {
-	cmd.AddManPagesCmd(rootCmd)
-	cmd.AddVersionCmd(rootCmd)
-
-	rootCmd.PersistentFlags().CountVarP(&verbosity, "verbosity", "v", "verbosity level (-v|-vv)")
-	rootCmd.PersistentFlags().BoolVarP(&quiet, "quiet", "q", false, "quiet mode")
-	rootCmd.MarkFlagsMutuallyExclusive("verbosity", "quiet")
-}
 
 func main() {
 	defer log.Sync()
+
+	rootCmd, err := cmd.GetRootCmd()
+	if err != nil {
+		log.Fatal().Msg(err.Error()).Send()
+	}
+
+	rootCmd.Use = "camserver-daemon"
+	rootCmd.Short = "camserver daemon"
+	rootCmd.Long = util.MultilineString(
+		"Camera Server daemon",
+		"",
+		"Handles everything in the backend.",
+	)
 
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal().Msg(err.Error()).Send()
