c03a91682202d667c0112b267bcdd13e52ea4538
AlexNg <contact@ngjx.org>
2026-04-01 21:59:32 +0800

    feat: Add feedmanager
    Signed-off-by: AlexNg <contact@ngjx.org>

---
+++ internal/feed/feed.go
@@ -0,0 +1,47 @@
+// Package feed handles the storage and intricacies of
+// having a minimal loss camera server.
+package feed
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"sync/atomic"
+
+	"github.com/caffeine-addictt/camserver/pkg/config"
+	"github.com/lattesec/log"
+)
+
+type FeedManager struct {
+	wg      *sync.WaitGroup
+	ctx     *context.Context
+	ctxDone context.CancelFunc
+
+	cameras    config.CameraCfg
+	archiveDir atomic.Value
+}
+
+// NewFeedManager follows the RAII model
+func NewFeedManager(ctx context.Context, rootWg *sync.WaitGroup) *FeedManager {
+	fmCtx, fmCtxDone := context.WithCancel(context.Background())
+
+	fm := &FeedManager{
+		wg:      &sync.WaitGroup{},
+		ctx:     &fmCtx,
+		ctxDone: fmCtxDone,
+	}
+
+	rootWg.Go(func() {
+		<-ctx.Done()
+		fm.Stop()
+		log.Info().Msg("shutting down feed manager").Send()
+		fmt.Println("shutting down feed manager")
+	})
+
+	return fm
+}
+
+func (fm *FeedManager) Stop() {
+	fm.ctxDone()
+	fm.wg.Wait()
+}
